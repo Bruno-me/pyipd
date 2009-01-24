232c1fccf244a41a27600e7da41bf7b3981e0932
Bruno_me <bruno.me@gmail.com>
2009-01-23 17:48:45 -0700

    It now tries to parse SMS, and is pretty good at it

--- reader.py
+++ reader.py
@@ -3,6 +3,10 @@
 import sys
 import struct
 import os.path
+import time
+
+def find_key(dic, val):
+	return [k for k, v in dic.iteritems() if v == val][0]
 
 filename = sys.argv[1]
 file = open(filename, "rb")
@@ -13,7 +17,7 @@ file.seek(0x28)
 
 #figure out the number of databases
 numdb = struct.unpack('h', file.read(2))[0]
-print numdb, "databases found"
+#print numdb, "databases found"
 
 databases = {}
 
@@ -22,26 +26,47 @@ for i in range(0,numdb):
 	namelen = struct.unpack("h", file.read(2))
 	databases[i] = file.read(namelen[0])[:-1]
 
-#print databases
+print databases
 
 records = ()
 
-while file.tell() < filesize:
+while file.tell() < (filesize - 1):
 	record = {}
-	record['dbid'] = struct.unpack("h", file.read(2))[0]
+	record['dbid'] = struct.unpack("H", file.read(2))[0]
 	rlength = struct.unpack("L", file.read(4))[0]
 	temptell = file.tell()
 	file.seek(file.tell() + 1)
-	record['handle'] = struct.unpack("h", file.read(2))[0]
+	record['handle'] = struct.unpack("H", file.read(2))[0]
 	record['uid'] = struct.unpack("L", file.read(4))[0]
-	#fields = file.read(rlength - 7)
 	record['fields'] = ()
-	while file.tell() < (temptell + rlength):
+	while file.tell() < (temptell + rlength) and file.tell() < filesize:
 		field = {}
-		flength = struct.unpack("h", file.read(2))[0]
+		flength = struct.unpack("H", file.read(2))[0]
 		field['type'] = struct.unpack("b", file.read(1))[0]
 		field['data'] = file.read(flength)
 		record['fields'] += ((field),)
+		if record['dbid'] == find_key(databases, 'SMS Messages') and field['type'] == 1:
+			received = 0
+			sent = 0
+			for i in range(13, 20):
+				temp = ord(field['data'][i])
+				received += temp << ((i - 13) * 8)
+			for i in range(21, 28):
+				temp = ord(field['data'][i])
+				#print 'Temp:	', temp
+				sent += temp << ((i - 21) * 8)
+			received = int(str(received)[:-3])
+			sent = int(str(sent)[:-3])
+			print 'received:	', time.ctime(received)
+			print 'sent:		', time.ctime(sent)
+		if record['dbid'] == find_key(databases, 'SMS Messages') and field['type'] == 4:
+			print 'text:		', field['data']
 	records += ((record,))
 
-print records
+for record in records:
+	print databases[record['dbid']]
+	print " Handle:", record['handle']
+	print " UID:", record['uid']
+	for field in record['fields']:
+		print "	Type:", field['type']
+		print "	Data:", field['data']

e9190446ff4f6a6db7c085cbe90fdfacac96111f
Bruno_me <bruno.me@gmail.com>
2010-03-26 02:17:11 -0600

    fixed up SMS date parsing, it didn't make sense and wasn't pythonic in the slightest. It's going to be object-based soon-ish anyway

--- reader.py
+++ reader.py
@@ -64,16 +64,9 @@ while file.tell() < (filesize - 1):
 		if record['dbid'] == find_key(databases, 'SMS Messages'):
 			#date field, unix timestamp
 			if field['type'] == 1:
-				received = 0
-				sent = 0
-				for i in range(13, 20):
-					received += ord(field['data'][i]) << ((i - 13) * 8)
-				for i in range(21, 28):
-					sent += ord(field['data'][i]) << ((i - 21) * 8)
-				#this is really horrible, but for some reason everything is shifted 3 decimals to the left
-				received = int(str(received)[:-3])
-				sent = int(str(sent)[:-3])
-				SMS['recieved'] = received
+				received = struct.unpack('Q', field['data'][13:21])[0] / 1000
+				sent = struct.unpack('Q', field['data'][21:29])[0] / 1000
+				SMS['received'] = received
 				SMS['sent'] = sent
 			#contents of the message
 			if field['type'] == 4:

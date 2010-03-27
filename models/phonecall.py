4a9c6c682806e8d7dd6e9160bc1ead90c42bf8c7
Bruno_me <bruno.me@gmail.com>
2010-03-26 19:53:26 -0600

    Done did the same thing to phone call logs as SMSs and the address book

---
+++ models/phonecall.py
@@ -0,0 +1,54 @@
+"""Handles Phone Call Logs"""
+
+import struct
+from datetime import datetime
+
+class Phonecall:
+	def __init__(self, fields, uid, handle):
+
+		self.uid = uid
+		self.handle = handle
+		self.fields = fields
+		self.decode()
+
+	def decode(self):
+		self.names = []
+		for field in self.fields:
+			if field['type'] == 4:
+				#timestamp, similar to SMS
+				self.calltime = datetime.fromtimestamp(struct.unpack('Q', field['data'])[0] / 1000)
+			if field['type'] == 12:
+				#phone number, includes a null at the end
+				self.number = field['data'][:-1]
+			if field['type'] == 31:
+				#name from address book
+				self.names += [field['data'][:-1],]
+			if field['type'] == 2:
+				#direction
+				direction = ord(field['data'][0])
+				if direction == 0:
+					self.direction = 'in'
+				elif direction == 1:
+					self.direction = 'out'
+				elif direction == 2:
+					self.direction = 'missed'
+				elif direction == 3:
+					self.direction = 'missed'
+				elif direction == 4:
+					self.direction = 'conference'
+				else:
+					self.direction = 'unknown - %d' % direction
+			if field['type'] == 3:
+				#duration (in seconds)
+				self.duration = struct.unpack('L', field['data'])[0]
+			if field['type'] == 6:
+				#failure code
+				failcode = ord(field['data'][0])
+				if failcode == 0:
+					self.disposition = 'Success'
+				elif failcode == 3:
+					self.disposition = 'Radio Path Unavailable'
+				elif failcode == 9:
+					self.disposition = 'Call Failed'
+				else:
+					self.disposition = 'unknown - %d' % failcode
--- reader.py
+++ reader.py
@@ -4,7 +4,7 @@ import sys
 import struct
 import os.path
 import time
-from models import addressbook, sms
+from models import addressbook, sms, phonecall
 
 def find_key(dic, val):
 	return [k for k, v in dic.iteritems() if v == val][0]
@@ -36,7 +36,7 @@ print databases
 
 records = ()
 SMSs = []
-Calls = ()
+Calls = []
 ABooks = []
 
 #Go the the file stopping at each record
@@ -49,8 +49,6 @@ while file.tell() < (filesize - 1):
 	record['handle'] = struct.unpack("H", file.read(2))[0]
 	record['uid'] = struct.unpack("L", file.read(4))[0]
 	record['fields'] = ()
-	if record['dbid'] == find_key(databases, 'Phone Call Logs'):
-		Call = {'uid': record['uid'], 'handle': record['handle'], 'time': 0, 'number': '', 'name': '', 'names': [], 'direction': '', 'duration': 0, 'disposition': ''}
 	while file.tell() < (temptell + rlength) and file.tell() < filesize:
 		field = {}
 		flength = struct.unpack("H", file.read(2))[0]
@@ -58,57 +56,11 @@ while file.tell() < (filesize - 1):
 		field['data'] = file.read(flength)
 		record['fields'] += ((field),)
 
-		if record['dbid'] == find_key(databases, 'Phone Call Logs'):
-			#timestamp, similar to SMS
-			if field['type'] == 4:
-				calltime = struct.unpack('Q', field['data'])[0]
-				Call['time'] = int(str(calltime)[:-3])
-			#phone number
-			if field['type'] == 12:
-				Call['number'] = field['data'][:-1]
-			#name from address book
-			if field['type'] == 31:
-				Call['names'] += [field['data'][:-1],]
-			#direction
-			if field['type'] == 2:
-				direction = ord(field['data'][0])
-				if direction == 0:
-					Call['direction'] = 'in'
-				elif direction == 1:
-					Call['direction'] = 'out'
-				elif direction == 2:
-					Call['direction'] = 'missed'
-				elif direction == 3:
-					Call['direction'] = 'missed'
-				elif direction == 4:
-					Call['direction'] = 'conference'
-				else:
-					Call['direction'] = 'unknown - %d' % direction
-			#duration (in seconds)
-			if field['type'] == 3:
-				Call['duration'] = struct.unpack('L', field['data'])[0]
-			#failure code
-			if field['type'] == 6:
-				failcode = ord(field['data'][0])
-				if failcode == 0:
-					Call['disposition'] = 'Success'
-				elif failcode == 3:
-					Call['disposition'] = 'Radio Path Unavailable'
-				elif failcode == 9:
-					Call['disposition'] = 'Call Failed'
-				else:
-					Call['disposition'] = 'unknown - %d' % failcode
 		if record['dbid'] == find_key(databases, 'Address Book - All') and field['type'] == 10:
 			#luckily, this is the only field type we're concerned with
 			ABooks.append(addressbook.ABook(field['data'], record['uid'], record['handle']))
 	records += (record,)
+	if record['dbid'] == find_key(databases, 'Phone Call Logs'):
+		Calls.append(phonecall.Phonecall(record['fields'], record['uid'], record['handle']))
 	if record['dbid'] == find_key(databases, 'SMS Messages'):
 		SMSs.append(sms.SMS(record['fields'], record['uid'], record['handle']))
-	if 'Call' in globals():
-		Call['uid'] = record['uid']
-		Call['handle'] = record['handle']
-		if not Call['number']:
-			Call['number'] = 'restricted'
-		Call['name'] = ' '.join(Call['names'])
-		Calls += (Call,)
-		del Call
